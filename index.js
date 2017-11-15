44319879a23b0ecaba596876bd45917c76d20b8c
pushqin <pushqin2@gmail.com>
2017-11-15 07:59:58 +0200

    Initial commit
    Webpack 3 ,react,babel,gitignore

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by alexgo on 11/14/2017.
+ */
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+
+const path                          = require('path');
+const distPath                      = path.join(__dirname, '..', 'dist');
+const config = {
+    entry: './index.js',
+    output: {
+        path:distPath,
+        filename: 'bundle.js',
+    },
+    devServer: {
+        inline: true,
+        port: 8080
+    },
+    module: {
+        loaders: [
+            {
+                test:/\.jsx?$/,
+                exclude:/node_modules/,
+                use:[
+                    {
+                        loader:'babel-loader',
+                        options:{
+                            presets:['es2015', 'react']
+                        },
+                    }
+                ]
+            }
+        ],
+
+    }
+};
+
+module.exports = config;
